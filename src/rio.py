d8ab147c87fdc778dac0b4a10a4ce828559b2010
Matteo Cassia <matteo.cassia@me.com>
2017-12-18 19:42:05 +0100

    Added sample file and dataset to test the accuracy of the classifier.

---
+++ src/rio.py
@@ -0,0 +1,34 @@
+import knn
+
+# Opens the file containing the data and extracts sex, height and weight;
+# The resulting list is in the form [["sex", (height, weight)], ...]
+
+a = []
+with open("../data/athletes.csv", "r") as file:
+    lines = file.readlines()
+    for line in lines:
+        elements = line.split(",")
+        try:
+            sex = str(elements[3])
+            height = float(elements[5])
+            weight = float(elements[6])
+            a.append([sex, (height, weight)])
+        except(ValueError, TypeError):
+            print("")
+
+# Tests and cross-validates the accuracy of the classifier
+t = 0
+f = 0
+for athlete in a:
+    target = athlete
+    samples = list(a)
+    samples.remove(target)
+    k = 100
+    kn = knn.KNN(samples, target[1], k)
+    c = kn.classify()
+    if c == target[0]:
+        t += 1
+    else:
+        f += 1
+    acc = (t / (t + f)) * 100
+    print(str(acc) + "%")
