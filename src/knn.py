0dda8dcb5ea70c9ff7cfddf2b38bd74ff44a40c2
Matteo Cassia <matteo.cassia@me.com>
2017-12-18 12:16:11 +0100

    Renamed parameters and functions, edited format of input data.

--- src/knn.py
+++ src/knn.py
@@ -3,55 +3,57 @@ import math
 
 class KNN:
 
-    def __init__(self, data, target, k):
+    def __init__(self, samples, target, k):
         """Initialises a KNN classifier
-        with training data as a dictionary containing class names and list of tuples containing numerical values
-            (i.e. {"male_footballers": [(84.5, 181, 21, ...), ...], ...}),
+        with training data as a list containing lists containing the classes and a tuple containing numerical values
+            (i.e. [["male_footballer": (84.5, 181, 21)], ...]),
         the target instance as a tuple containing numerical values
             (i.e. (75, 169, 34)) to be classified and
         the number of nearest neighbours to be considered as an integer
             (i.e. 3)."""
-        self.data = data
+        self.samples = samples
         self.target = target
         self.k = k
 
-    def fit(self):
-        """Returns the string representing the name of the class
-        which the target instance has been classified to."""
-        neighbours = self.get_neighbours()
-        nearest_neighbours = sorted(neighbours, key=lambda t: t[1])[: self.k]
-        majority_class_name = self.get_majority_class_name(nearest_neighbours)
-        return majority_class_name
+    def classify(self):
+        """Returns the class which the target instance has been classified to."""
+        sample_distances = self.get_sample_distances()
+        nearest_neighbours = map(lambda a: a[1], sample_distances[:self.k])
+        majority_class = self.get_majority_class(nearest_neighbours)
+        return majority_class
 
-    def get_neighbours(self):
-        """It returns all the neighbours as tuples containing the class name and distance
-        of the target instance."""
-        neighbours = []
-        for class_name in self.data:
-            for instance in self.data[class_name]:
-                neighbours.append((class_name, self.get_distance(self.target, instance)))
-        return neighbours
+    def get_sample_distances(self):
+        """It returns all the samples associated with their distance to the target
+        as a list containing distance and sample."""
+        sample_distances = []
+        for sample in self.samples:
+            distance = self.get_distance(self.target, sample[1])
+            sample_distances.append((distance, sample))
+        return sorted(sample_distances)
 
     @staticmethod
-    def get_majority_class_name(neighbours):
-        """Given a list of neighbours as a tuple containing the class name and distance,
+    def get_majority_class(neighbours):
+        """Given a list of samples,
         it returns the most recurring class name of the neighbours."""
-        class_names = [n[0] for n in neighbours]
-        best_class_name = None
-        occurrences = -1
-        for class_name in class_names:
-            x = class_names.count(class_name)
-            if x > occurrences:
-                best_class_name = class_name
-                occurrences = x
-        return best_class_name
+        counts = {}
+        for neighbour in neighbours:
+            if neighbour[0] not in counts:
+                counts[neighbour[0]] = 0
+            counts[neighbour[0]] += 1
+        majority_count = -1
+        majority_class = None
+        for c in counts:
+            if counts[c] > majority_count:
+                majority_count = counts[c]
+                majority_class = c
+        return majority_class
 
     @staticmethod
     def get_distance(a, b):
         """Given two n-dimensional points as tuples of numerical values,
         it returns the (euclidean) distance between the two."""
-        sum = 0
+        t = 0
         for i in range(0, len(a)):
             d = pow(abs(a[i] - b[i]), 2)
-            sum += d
-        return math.sqrt(sum)
+            t += d
+        return math.sqrt(t)
--- tests/tests.py
+++ tests/tests.py
@@ -4,38 +4,32 @@ import knn as my_knn
 class Test(unittest.TestCase):
 
     def setUp(self):
-        data  = {
-            "bottom_left": [
-                (0.5, 1.0),
-                (1.0, 1.5),
-                (1.0, 2.0)],
-            "top_right": [
-                (9.5, 8.0),
-                (8.0, 9.5),
-                (9.0, 10.0)],
-            "top_left": [
-                (0.5, 8.0),
-                (1.0, 9.5),
-                (1.0, 10.0)]
-        }
+        samples = [
+            ["bottom_left", (0.5, 1.0)],
+            ["bottom_left", (1.5, 1.0)],
+            ["bottom_left", (1.5, 1.5)],
+            ["top_right", (9.5, 8.0)],
+            ["top_right", (8.5, 7.0)],
+            ["top_right", (10.5, 9.0)],
+        ]
         k = 2
-        target = (2.0, 9.0)
-        knn = my_knn.KNN(data, target, k)
+        target = (2.0, 3.0)
+        knn = my_knn.KNN(samples, target, k)
         self.knn = knn
 
-    def test_get_majority_class(self):
-        self.assertEqual(self.knn.fit(), "top_left")
+    def test_classify(self):
+        self.assertEqual(self.knn.classify(), "bottom_left")
 
     def test_get_distance(self):
         point_a = (3.0, 4.0)
         point_b = (6.0, 0.0)
         self.assertEqual(self.knn.get_distance(point_a, point_b), 5.0)
 
-    def test_get_majority_class_name(self):
+    def test_get_majority_class(self):
         neighbours = [
-            ("blue", (1.0, 2.0)),
-            ("green", (1.0, 2.0)),
-            ("green", (1.0, 2.0)),
-            ("green", (1.0, 2.0)),
+            ["blue", (1.0, 2.0)],
+            ["green", (1.0, 2.0)],
+            ["green", (1.0, 2.0)],
+            ["green", (1.0, 2.0)],
         ]
-        self.assertEqual(self.knn.get_majority_class_name(neighbours), "green")
+        self.assertEqual(self.knn.get_majority_class(neighbours), "green")
